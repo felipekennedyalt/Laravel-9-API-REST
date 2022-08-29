e53ed7079ef8db08c172863d40ca3cdb5045eeb0
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2022-08-29 15:58:27 -0300

    rota api

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function () {
-    return 'felipe kennedy';
-});
+Route::get('/products', [ProductController::class, 'index']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
